c7d89b4d4b03d145b97104565d04a15293252312
erdoganishe <khotyanmisha@gmail.com>
2023-11-22 11:39:33 +0200

    project created

---
+++ eliptic_curve_rust/target/debug/deps/eliptic_curve_rust-3dc2928999a78b09.d
@@ -0,0 +1,5 @@
+C:\Users\khoty\Documents\Projects\ElipticCurveRust\ElipticCurveRust\eliptic_curve_rust\target\debug\deps\eliptic_curve_rust-3dc2928999a78b09.rmeta: src\main.rs
+
+C:\Users\khoty\Documents\Projects\ElipticCurveRust\ElipticCurveRust\eliptic_curve_rust\target\debug\deps\eliptic_curve_rust-3dc2928999a78b09.d: src\main.rs
+
+src\main.rs:
---
+++ eliptic_curve_rust/target/debug/deps/eliptic_curve_rust-ddb546282ac99fb2.d
@@ -0,0 +1,5 @@
+C:\Users\khoty\Documents\Projects\ElipticCurveRust\ElipticCurveRust\eliptic_curve_rust\target\debug\deps\eliptic_curve_rust-ddb546282ac99fb2.rmeta: src\main.rs
+
+C:\Users\khoty\Documents\Projects\ElipticCurveRust\ElipticCurveRust\eliptic_curve_rust\target\debug\deps\eliptic_curve_rust-ddb546282ac99fb2.d: src\main.rs
+
+src\main.rs:
